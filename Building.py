2b3bda415f11a7dbc428bdb11cb77281c114cd31
Krystian Lalik <klalik27@gmail.com>
2019-11-02 16:59:27 +0100

    Made building size more scalable with screen size. Added health and mentality bar to the interface

--- Building.py
+++ Building.py
@@ -4,7 +4,7 @@ import pygame
 class Building(pygame.Rect):
     def __init__(self, screen, screenSize):
         self.screenWidth, self.screenHeight = screenSize
-        self.constraints = self.leftWallX, self.rightWallX, self.floorY, self.ceilingY = 100, 850, self.screenHeight - 95, 100
+        self.constraints = self.leftWallX, self.rightWallX, self.floorY, self.ceilingY = 100, self.screenWidth - 100, self.screenHeight - 95, 100
         pygame.Rect.__init__(self, (self.leftWallX, self.ceilingY, self.rightWallX - self.leftWallX, self.floorY - self.ceilingY))
         self.screen = screen
 
--- Interface.py
+++ Interface.py
@@ -8,7 +8,9 @@ class Interface(object):
         self.equipment1 = InterfacePart(screen, screenX//2 - 100, screenY - 26, "Weapon: ", "Hands")
         self.equipment2 = InterfacePart(screen, screenX//2 + 100, screenY - 26, "Usable: ", "Bread")
         self.health = InterfacePart(screen, 3, screenY - 26, "Health: ", "50/100", (204, 0, 0))
+        self.healthBar = StatusIndicator(screen, 3, screenY - 30, 130, 50, (204, 0, 0))
         self.mentality = InterfacePart(screen, screenX - 160, screenY - 26, "Mentality: ", "30/100", (0, 0, 102))
+        self.mentalityBar = StatusIndicator(screen, screenX - 160, screenY - 34, 155, 30, (0, 0, 102))
 
     def update(self):
         self.score.update()
@@ -16,7 +18,9 @@ class Interface(object):
         self.equipment1.update()
         self.equipment2.update()
         self.health.update()
+        self.healthBar.update()
         self.mentality.update()
+        self.mentalityBar.update()
 
 class InterfacePart(pygame.Rect):
     def __init__(self, screen, positionX, positionY, indicator, information, color = (255, 255, 255)):
@@ -34,3 +38,27 @@ class InterfacePart(pygame.Rect):
     def setInformation(self, information):
         self.information = information
         self.surf = self.font.render((self.indicator + self.information), True, self.color)
+
+
+class StatusIndicator(pygame.Rect):
+    def __init__(self, screen, positionX, positionY, barWidth, value, color = (255, 255, 255)):
+        self.screen = screen
+        self.maxValue = 100
+        self.ratio = barWidth / self.maxValue
+        print(self.ratio)
+        self.value = value
+        self.width = self.value * self.ratio
+        self.color = color
+        pygame.Rect.__init__(self, (positionX, positionY, self.width, 5))
+        self.statusBar = pygame.Rect((positionX - 1, positionY - 1, barWidth + 2, 7))
+
+    def update(self):
+        pygame.draw.rect(self.screen, (0,0,0), self.statusBar)
+        pygame.draw.rect(self.screen, self.color, self)
+
+    def setValue(self, value):
+        self.value = value
+        self.width = self.value * self.ratio
+
+    def getValue(self):
+        return self.value
