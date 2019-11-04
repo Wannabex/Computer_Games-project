f68804707ddaca7fa098be22758e034237c64d5a
Krystian Lalik <klalik27@gmail.com>
2019-11-04 20:32:16 +0100

    Prepared Equipment class with 3 weapons and 4 consumables ready to be implemented

---
+++ Equipment.py
@@ -0,0 +1,82 @@
+import pygame
+
+
+class Weapon(pygame.Rect):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class Sword(Weapon):
+    def __init__(self):
+        Weapon.__init__()
+
+    def update(self):
+        pass
+
+
+class Whip(Weapon):
+    def __init__(self):
+        Weapon.__init__()
+
+    def update(self):
+        pass
+
+
+class Shield(Weapon):
+    def __init__(self):
+        Weapon.__init__()
+
+    def update(self):
+        pass
+
+
+
+
+
+
+class Consumable(pygame.Rect):
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class Bomb(Consumable):
+    def __init__(self):
+        Consumable.__init__()
+
+    def update(self):
+        pass
+
+
+class Flute(Consumable):
+    def __init__(self):
+        Consumable.__init__()
+
+    def update(self):
+        pass
+
+
+class Garlic(Consumable):
+    def __init__(self):
+        Consumable.__init__()
+
+    def update(self):
+        pass
+
+
+class Rune(Consumable):
+    def __init__(self):
+        Consumable.__init__()
+
+    def update(self):
+        pass
+
+
+
+
+
